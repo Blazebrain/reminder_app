241347345279bcb7870b0c875aff682aeecdb24d
Kaushal-Vasava <kaushal.vasava@algozen.in>
2023-06-29 15:30:18 +0530

    Update code for android version upto android 13 and update layout
    - Fix bugs on date selection
    - Add notification permission
    - Add permission request and check logic

--- app/src/main/java/com/blazebrain/reminderapp/util/NotificationHelper.kt
+++ app/src/main/java/com/blazebrain/reminderapp/util/NotificationHelper.kt
@@ -1,27 +1,35 @@
 package com.blazebrain.reminderapp.util
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.os.Build
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.blazebrain.reminderapp.MainActivity
 import com.blazebrain.reminderapp.R
 
-class NotificationHelper(val context: Context ) {
-    private  val CHANNEL_ID  = "reminder_channel_id"
-    private  val NOTIFICATION_ID = 1
+class NotificationHelper(private val context: Context) {
+    companion object {
+        private const val CHANNEL_ID = "reminder_channel_id"
+        private const val NOTIFICATION_ID = 1
+    }
 
-    fun createNotification(title: String, message: String){
+    @SuppressLint("MissingPermission")
+    fun createNotification(title: String, message: String) {
         createNotificationChannel()
-        val intent = Intent(context, MainActivity:: class.java).apply{
+        val intent = Intent(context, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
-        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        val pendingIntent =
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         val icon = BitmapFactory.decodeResource(context.resources, R.drawable.reminder_char)
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.checklist)
@@ -29,22 +37,39 @@ class NotificationHelper(val context: Context ) {
             .setContentTitle(title)
             .setContentText(message)
             .setStyle(
-                NotificationCompat.BigPictureStyle().bigPicture(icon).bigLargeIcon(null)
+                NotificationCompat.BigPictureStyle().bigPicture(icon)
             )
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
-        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification)
-
+        if (isPermissionGranted(context)) {
+            NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notification)
+        }
     }
 
-    private fun createNotificationChannel(){
+    private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-           val channel = NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT ).apply {
-               description = "Reminder Channel Description"
- }
-            val notificationManager =  context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_ID,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Reminder"
+            }
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
-       }
+        }
+    }
+}
+
+fun isPermissionGranted(context: Context): Boolean {
+    return if (Build.VERSION_CODES.TIRAMISU <= Build.VERSION.SDK_INT) {
+        ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) == PackageManager.PERMISSION_GRANTED
+    } else {
+        true
     }
 }
--- app/src/main/java/com/blazebrain/reminderapp/util/ReminderWorker.kt
+++ app/src/main/java/com/blazebrain/reminderapp/util/ReminderWorker.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
-class ReminderWorker(val context: Context, val params: WorkerParameters) : Worker(context, params){
+class ReminderWorker(
+    private val context: Context, params: WorkerParameters) :
+    Worker(context, params) {
     override fun doWork(): Result {
         NotificationHelper(context).createNotification(
-        inputData.getString("title").toString(),
-            inputData.getString("message").toString())
+            inputData.getString("title").toString(),
+            inputData.getString("message").toString()
+        )
 
         return Result.success()
     }
